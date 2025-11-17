cac710a82ffb1f53db9a58a2a98b37cfa98f2715
bindu-19-code <bindukreddy1111@gmail.com>
2025-11-17 09:52:10 +0530

    ABCDEF

--- backend/app.py
+++ backend/app.py
@@ -114,6 +114,7 @@ history_collection = db["history"]
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
 app.config["MAIL_USERNAME"] = os.environ.get("EMAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.environ.get("EMAIL_PASSWORD")
 mail = Mail(app)
